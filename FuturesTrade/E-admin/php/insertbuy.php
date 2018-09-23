1198c860437bf2f4b0d2f7880468de16aea0bc8f
ulihgu <ulihgu@msn.ocm>
2018-09-23 23:38:05 +0800

    2018-9-23_slh_1

--- FuturesTrade/E-admin/php/insertbuy.php
+++ FuturesTrade/E-admin/php/insertbuy.php
@@ -1,7 +1,6 @@
 <?php
     ini_set("error_reporting",E_ALL ^ E_NOTICE); 
     //include "../../php/sqlConfig.php";
-
     $buyAmount = $_POST['buyAmount'];//数量
     $dealPrice = $_POST['dealPrice'];//成交价格
     $index_xt = $_POST['index_xt'];//形态
@@ -13,8 +12,14 @@
     $stockCode = $_POST['stockCode'];
     $stockName = $_POST['stockName'];
     $upward = $_POST['upward'];
+    $daysNumber = $_POST['daysNumber'];
+    $enlarge = $_POST['enlarge'];
+    $middleUpward = $_POST['middleUpward'];
+    $rails = $_POST['rails'];
+    $vaginalLine = $_POST['vaginalLine'];
+    $stopFalling = $_POST['stopFalling'];
 
-    $time = date("Y-m-d",time());
+    $time = date('Y-m-d H:i:s');
 
     /* $mysql_server_name = 'cdb-g2r69g0e.bj.tencentcdb.com:10018';
     $mysql_username = 'root';
@@ -24,15 +29,15 @@
     $sqlConnect = new PDO($dsn, $mysql_username, $mysql_password); */
     include "../../php/sqlConfig.php";
     
-    $sql = "insert into shares_stock(index_zb,index_xt,shares_amount,shares_price,positives,priceLoss,shrinkage,smallK,shares_code,shares_name,upward,time) 
-    values('{$index_zb}','{$index_xt}','{$buyAmount}','{$dealPrice}','{$positives}','{$priceLoss}','{$shrinkage}','{$smallK}','{$stockCode}','{$stockName}','{$upward}',{$time})";
+    $sql = "insert into shares_stock(index_zb,index_xt,shares_amount,shares_price,positives,priceLoss,shrinkage,smallK,shares_code,shares_name,upward,daysNumber,enlarge,middleUpward,rails,vaginalLine,stopFalling,time)
+    values('{$index_zb}','{$index_xt}','{$buyAmount}','{$dealPrice}','{$positives}','{$priceLoss}','{$shrinkage}','{$smallK}','{$stockCode}','{$stockName}','{$upward}','{$daysNumber}','{$enlarge}','{$middleUpward}','{$rails}','{$vaginalLine}','{$stopFalling}','{$time}')";
     $query = $sqlConnect->prepare($sql);    
     
     $arrs = array();
     if($query->execute()){
-        $arrs = array('error' =>'正常:没有问题！','state'=>'OK');
+        $arrs = array('error' =>'正常:没有问题！'.$middleUpward,'state'=>'OK');
     }else{
-        $arrs = array('error' => '错误:写入数据库失败！', 'state' => 'NO');
+        $arrs = array('error' => '错误:写入数据库失败！', 'state' => 'NO','time' => $time);
     }
     echo json_encode($arrs);
 ?>
--- FuturesTrade/php/queryDateData.php
+++ FuturesTrade/php/queryDateData.php
@@ -1,54 +1,51 @@
 <?php
  ini_set("error_reporting",E_ALL ^ E_NOTICE);
  //$state='2';
-
+ $arrs = array();
  if (isset($_POST['mydate'])) {
      $mydate = $_POST['mydate'];
  }
  $startTime = date('Y-m-d 00:00:00');
  $endTime = date('Y-m-d H:i:s');
  //查询所有用户数据
- function fetchData($sql)
- {       
-     /* $mysql_server_name = 'cdb-g2r69g0e.bj.tencentcdb.com:10018';
-     $mysql_username = 'root';
-     $mysql_password = 'ulihgu21';
-     $mysql_database = 'mysqldatabase';
-     $dsn='mysql:host='.$mysql_server_name.';dbname='.$mysql_database.';';
-     $dbcon=new PDO($dsn,$mysql_username,$mysql_password); */
-     include "sqlConfig.php";
-     //连接数据库phpbos\mysql1\api.php
-    try{             
-        if($sqlConnect){
-            /* $stmt = $dbcon->prepare($sql);    
-            $stmt->execute();
-            $result = $stmt->fetchAll(PDO::FETCH_OBJ);      */ 
-            //查询结果集
-            //$query = $sqlConnect->query($sql);
-            $query = $sqlConnect->prepare($sql);
-            $query->execute();
-            $arrs = $query->fetchAll(PDO::FETCH_ASSOC);
-        }else{
-         $arrs = array('error' => '数据连接失败！', 'state' => 'NO'); 
+        function fetchData($sql)
+            {       
+                $mysql_server_name = 'cdb-g2r69g0e.bj.tencentcdb.com:10018';
+                $mysql_username = 'root';
+                $mysql_password = 'ulihgu21';
+                $mysql_database = 'mysqldatabase';
+                $dsn = 'mysql:host='.$mysql_server_name.';dbname='.$mysql_database.';';
+                $sqlConnect = new PDO($dsn, $mysql_username, $mysql_password);
+                //include "sqlConfig.php";
+                //连接数据库phpbos\mysql1\api.php
+                try{             
+                    if($sqlConnect){
+                        /* $stmt = $dbcon->prepare($sql);    
+                        $stmt->execute();
+                        $result = $stmt->fetchAll(PDO::FETCH_OBJ);      */ 
+                        //查询结果集
+                        //$query = $sqlConnect->query($sql);
+                        $query = $sqlConnect->prepare($sql);
+                        $query->execute();
+                        $arrs = $query->fetchAll(PDO::FETCH_ASSOC);
+                    }else{
+                    $arrs = array('error' => '数据连接失败！', 'state' => 'NO'); 
+                    }
+                }catch(PDOException $e){
+                $arrs = array('error' => '数据连接失败！', 'state' => 'NO'); 
+                }
+                echo json_encode($arrs);
         }
-    }catch(PDOException $e){
-     $arrs = array('error' => '数据连接失败！', 'state' => 'NO'); 
-    }
-    echo json_encode($arrs);
- }
 
- if($state == '1'){
-    $sql = "SELECT * FROM shares_stock WHERE time >= unix_timestamp( '$startTime' ) AND time <= unix_timestamp( '$endTime' )";       
-    //fetchData($sql);   
- }
-/*  if($state == '0'){
-    $sql = "SELECT id,user,city,email,joinTime,isadmin FROM account WHERE isadmin='0'";       
+
+    //$startTime = unix_timestamp( '$startTime' );
+    //$endTime = unix_timestamp( '$endTime' );
+    $sql = "SELECT shares_code,shares_name,shares_price,shares_amount,priceLoss,time FROM shares_stock WHERE time >= '{$startTime}' AND time <= '{$endTime}'";       
     fetchData($sql);   
- }
- if($state == '2'){
-    $sql = "SELECT id,user,city,email,joinTime,isadmin FROM account";       
-    fetchData($sql);   
- } */
- $arrs = array('error' => '正常:查询股票数据失败！', 'state' => 'NO');
- echo json_encode($arrs);
+    //$startTime2 = strtotime($startTime);
+    //$endTime2 = strtotime($endTime);
+    //$startTime3 = date("Y-m-d H:i:s",$startTime2);
+    //$endTime3 = date("Y-m-d H:i:s",$endTime2);
+    //$arrs = array('error' => '测试:查询股票数据失败！', 'state' => 'NO', 'startTime' => $startTime, 'endTime' => $endTime, '时间戳开始' => $startTime2, '时间戳结束' => $endTime2 , '时间戳转回开始' => $startTime3, '时间戳转回结束' => $endTime3);
+    //echo json_encode($arrs);
 ?>
